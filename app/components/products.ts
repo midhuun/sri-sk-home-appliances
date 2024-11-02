b8fe718c40b9fdd48f1abcd91b23b45f87622b10
Midhun <midhun2031@gmail.com>
2024-11-02 10:18:34 +0530

    Fixed Database Bug

--- app/components/products.ts
+++ app/components/products.ts
@@ -1,6 +1,6 @@
 export async function getItem() {
      try{
-    const res = await fetch('https://sk-home-appliancess.vercel.app/api/user', { next: { revalidate: 100 } },);
+    const res = await fetch('http://127.0.0.1:3000/api/user', { next: { revalidate: 100 } },);
         return res.json()}
         catch (error) {  console.error(error); }
 
